da565c61ffd8e0f74d28d02f65e8478425d48440
Lê Nguyễn An Ninh <leninh2004@gmail.com>
2026-03-08 16:08:58 +0700

    Refactor CORS configuration to use allowedOriginPatterns for improved flexibility

--- src/main/java/com/edutool/config/CorsConfig.java
+++ src/main/java/com/edutool/config/CorsConfig.java
@@ -12,8 +12,8 @@ public class CorsConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**") 
-                        .allowedOrigins("http://localhost:5173","https://edu-tool-fe.vercel.app/")
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("http://localhost:*", "https://edu-tool-fe.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
